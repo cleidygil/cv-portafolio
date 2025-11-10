ecee213374677eb91963a6ba26fa3e7473a70b0b
cleidygil <cleidygil42@gmail.com>
2025-11-10 15:32:05 -0400

    UPDATED FILES .git/COMMIT_EDITMSG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,28 +34,28 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "favicon/favicon-16x16.png",
+        url: "/public/favicon/favicon-16x16.png",
         sizes: "any",
       },
       {
-        url: "favicon/favicon-16x16.png",
+        url: "/public/favicon/favicon-16x16.png",
         type: "image/png",
         sizes: "32x32",
       },
       {
-        url: "favicon/android-chrome-192x192.png",
+        url: "/public/favicon/android-chrome-192x192.png",
         type: "image/png",
         sizes: "192x192",
       },
       {
-        url: "favicon/android-chrome-512x512.png",
+        url: "/public/favicon/android-chrome-512x512.png",
         type: "image/png",
         sizes: "512x512",
       },
     ],
     apple: [
       {
-        url: "favicon/apple-touch-icon.png",
+        url: "/public/favicon/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
       },
